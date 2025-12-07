3b3ed049c995eb3f57a9faae315cf94e6fe6ae32
Daniel Yoann <64357912+F4IGV@users.noreply.github.com>
2025-12-07 11:56:21 +0100

    Update run_dapnet_tropo_alert.bat with folder comment
    Added a comment to specify the folder containing the .py file.

--- run_dapnet_tropo_alert.bat
+++ run_dapnet_tropo_alert.bat
@@ -1,4 +1,5 @@
 @echo off
 REM Lance le script Python pour envoyer la météo solaire sur DAPNET
-cd /d "C:\Scripts\DAPNET"
+cd /d "C:\Scripts\DAPNET"  #choose your folder who contain the .py file
 py "dapnet_tropo_alert.py" >> "C:\Scripts\DAPNET\dapnet_tropo_alert.log" 2>&1
+
